cc8e11bd635d5f6e38842ee8b3898078f1c1dc67
Anatoliy-Nelson <Sirius_Star@inbox.ru>
2024-10-15 01:17:55 +0300

    init

---
+++ src/sections/Main.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export Main = () => {
+    return (
+        <div>
+            <div>
+                <h2>
+                    Headline
+                </h2>
+                <p>
+                    Faucibus. Faucibus. Sit sit sapien sit tempusrisu ut. Sit molestie ornare in venen.
+                </p>
+            </div>
+        </div>
+    );
+};
